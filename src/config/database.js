04584e94ae4ff00a7aa17ffd26e4a1a046d1dc74
Ayush-0312 <kumarayush3121511@gmail.com>
2025-06-12 13:29:30 +0530

    database error console

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  console.log("connecting to db" + process.env.DB_CONNECTION_SECRET);
   await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 };
 
