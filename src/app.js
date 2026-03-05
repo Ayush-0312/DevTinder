257bb66e06e9fa9c263828125b578325bc05687b
Ayush-0312 <kumarayush3121511@gmail.com>
2026-03-05 22:47:48 +0530

    socket connection

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/database");
 const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
+const http = require("http");
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
@@ -13,16 +14,20 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/requests");
 const userRouter = require("./routes/user");
+const initializeSocket = require("./utils/socket");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 
+const server = http.createServer(app);
+initializeSocket(server);
+
 connectDB()
   .then(() => {
     console.log("Database connection established successfully");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("Successfully listening...");
     });
   })
